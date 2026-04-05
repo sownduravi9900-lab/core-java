d9485cf98fa25f15e07ee8592f41b4a2f908c7aa
sownduravi9900-lab <sownduravi9900@gmail.com>
2026-04-05 15:04:09 +0530

    methods

---
+++ Students.java
@@ -0,0 +1,11 @@
+class Students{
+public static void main(String[] args){
+String names;
+int marks;
+Students names("Sowndarya","Boomika","Yashwanth","Revanth");
+Students marks("456",478,"345","564");
+}
+System.out.println("Students name:" + names);
+System.out.println("Students marks:" + marks);
+
+}
